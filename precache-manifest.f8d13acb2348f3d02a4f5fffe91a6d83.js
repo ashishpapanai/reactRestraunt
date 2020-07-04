ca6569b64353eee3eba9215c46284ac77f2c0585
Ashish Papanai <ashishpapanai00@gmail.com>
2020-07-05 00:16:29 +0530

    Updates

---
+++ precache-manifest.f8d13acb2348f3d02a4f5fffe91a6d83.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e758f791adf5978a337dab90a9f19e97",
+    "url": "/reactRestraunt/index.html"
+  },
+  {
+    "revision": "78ea0586e250619c2545",
+    "url": "/reactRestraunt/static/css/2.a095ab49.chunk.css"
+  },
+  {
+    "revision": "5dcb00eaf89e762801b6",
+    "url": "/reactRestraunt/static/css/main.95a77cd4.chunk.css"
+  },
+  {
+    "revision": "78ea0586e250619c2545",
+    "url": "/reactRestraunt/static/js/2.6e991296.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/reactRestraunt/static/js/2.6e991296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dcb00eaf89e762801b6",
+    "url": "/reactRestraunt/static/js/main.4ec40dcd.chunk.js"
+  },
+  {
+    "revision": "ce93b384901ef535389a",
+    "url": "/reactRestraunt/static/js/runtime-main.de1020b8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactRestraunt/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactRestraunt/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactRestraunt/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactRestraunt/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactRestraunt/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
